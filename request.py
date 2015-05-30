fc9c80232f85277f5f4db204c774e0d41914fec5
MAHA06 <mihai_mahalean@yahoo.com>
2015-05-30 15:34:18 +0300

    Request/VizuaLIZATOR/

--- request.py
+++ request.py
@@ -1,5 +1,6 @@
 import urllib2
 
 def request(x, y):
-    conn = urllib2.urlopen("http://battleship.pitechplus.com/game/shoot/team9321?coordinates=" + str(x) + str(y))
+    g=str(chr(x+ord('A')-1))
+    conn = urllib2.urlopen("http://battleship.pitechplus.com/game/shoot/team9321?coordinates=" + g + str(y))
     return conn.read(100)
