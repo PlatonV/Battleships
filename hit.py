92c9de47a14ab4335d18aafaa1d9cf2d321de27b
MAHA06 <mihai_mahalean@yahoo.com>
2015-05-30 14:10:22 +0300

    Bug

--- hit.py
+++ hit.py
@@ -1,33 +1,32 @@
 from random import randint
+import request
 import pygame
 from pygame.locals import *
+import startNewGame
+
 
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 lovit=[]
 def hit(x,y):
-	print(x+1,y+1)	
-	if (s[x][y]=="1") :
+	print("Am pornit")
+	print(x,y)	
+	if (s[x-1][y-1]=="1") :
 		print("lovit")
 	for i in range(0,4):
 		m,n=xoy[i]
-		if(x+m<10)&(y+n<10)&(x+m>-1)&(y+n>-1):
-			if (s[x+m][y+n]=="1") & ((x+m,y+n) not in lovit):
-			
-				lovit.append((m+x,n+y))
+		if(x+m<11)&(y+n<11)&(x+m>-1)&(y+n>-1):
+			if (request.request(chr(x+m-1+ord('A')),y+n+1)in["HIT","DESTROYED"]) & ((x+m,y+n) not in lovit):		
 				hit(x+m,y+n)
-		
-				
+		lovit.append((m+x,n+y))
 
-
-
-
-print("Am pornit")
 
 running = True
+startNewGame.startNewGame()
 xoy=[(0,-1),(1,0),(0,1),(-1,0)]
 while running:
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			running = False
@@ -37,15 +36,21 @@ while running:
 	s=file.readlines()
 	
 
-	while ok:
+	while len(lovit)<100:
 		
-		x= randint(0,9)
-		y=randint(0,9)
-		if (s[x][y]=="1") & ((x,y) not in lovit):
+		x= randint(1,10)
+		y=randint(1,10)
+		g=chr(x+ord('A')-1)
+		if (request.request(g,y)in["HIT","DESTROYED"]) & ((x,y) not in lovit):
+
+			
 			lovit.append((x,y))
 			hit(x,y)
+		print(request.request(g,y)+str(g)+str(y))
 
-		ok=False
+
+		
+		
 
 
 
